5cbf45c06678e7d4fe61698bce8672d2f7bb3ef1
petuhovskiy <petuhovskiy@yandex.ru>
2018-04-12 11:41:03 +0300

    hello world

---
+++ src/main/kotlin/io/rwlist/Main.kt
@@ -0,0 +1,23 @@
+package io.rwlist
+
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main(args: Array<String>) {
+
+    val server = embeddedServer(Netty, port = 9000) {
+        routing {
+            get("/") {
+                call.respondText("Hello World!", ContentType.Text.Plain)
+            }
+            get("/demo") {
+                call.respondText("HELLO WORLD!")
+            }
+        }
+    }
+    server.start(wait = true)
+}
